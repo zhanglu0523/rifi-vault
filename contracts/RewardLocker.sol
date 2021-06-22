434e1b14a839f5ccd288b66537aa63263ec5aa37
thanzou <thanzou@users.noreply.github.com>
2021-06-22 09:22:09 +0700

    Vault version 2
    - Add reward locker for vesting
    - Freeze/unfreeze vault
    - Remove Claim func (use Reward locker instead)
    - Tracking share for future compounding deposit
    - Reintegrate with OpenZeppelin libraries version 3.4
    - Reorganize and refactor internal funcs
    - Improve condition check in internal funcs
    - Migrate to Solidity v0.7.6 (was v0.5.16)

---
+++ contracts/RewardLocker.sol
@@ -0,0 +1,356 @@
+// SPDX-License-Identifier: agpl-3.0
+
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+
+import "./libraries/token/IERC20.sol";
+import "./libraries/math/SafeMath.sol";
+import "./libraries/utils/SafeCast.sol";
+import "./libraries/token/SafeERC20.sol";
+import "./libraries/utils/EnumerableSet.sol";
+import "./libraries/access/PermissionAdmin.sol";
+
+import {IRewardLocker} from './IRewardLocker.sol';
+
+contract RewardLocker is IRewardLocker, PermissionAdmin {
+  using SafeMath for uint256;
+  using SafeCast for uint256;
+
+  using SafeERC20 for IERC20;
+  using EnumerableSet for EnumerableSet.AddressSet;
+
+  struct VestingSchedules {
+    uint256 length;
+    mapping(uint256 => VestingSchedule) data;
+  }
+
+  uint256 private constant MAX_REWARD_CONTRACTS_SIZE = 10;
+
+  /// @dev whitelist of reward contracts
+  mapping(IERC20 => EnumerableSet.AddressSet) internal rewardContractsPerToken;
+
+  /// @dev vesting schedule of an account
+  mapping(address => mapping(IERC20 => VestingSchedules)) private accountVestingSchedules;
+
+  /// @dev An account's total escrowed balance per token to save recomputing this for fee extraction purposes
+  mapping(address => mapping(IERC20 => uint256)) public accountEscrowedBalance;
+
+  /// @dev An account's total vested reward per token
+  mapping(address => mapping(IERC20 => uint256)) public accountVestedBalance;
+
+  /// @dev Total amount escrowed since creation for each token
+  mapping(IERC20 => uint256) public lifetimeLockedTotal;
+
+  /// @dev Total amount vested since creation for each token
+  mapping(IERC20 => uint256) public lifetimeVestedTotal;
+
+  /// @dev vesting duration for each token
+  mapping(IERC20 => uint256) public vestingDurationPerToken;
+
+  /* ========== EVENTS ========== */
+  event RewardContractAdded(address indexed rewardContract, bool isAdded);
+  event SetVestingDuration(IERC20 indexed token, uint64 vestingDuration);
+
+  /* ========== MODIFIERS ========== */
+
+  modifier onlyRewardsContract(IERC20 token) {
+    require(rewardContractsPerToken[token].contains(msg.sender), 'only reward contract');
+    _;
+  }
+
+  constructor(address _admin) PermissionAdmin(_admin) {}
+
+  /**
+   * @notice Add a whitelisted rewards contract
+   */
+  function addRewardsContract(IERC20 token, address _rewardContract) external onlyAdmin {
+    require(
+      rewardContractsPerToken[token].length() < MAX_REWARD_CONTRACTS_SIZE,
+      'rewardContracts is too long'
+    );
+    require(rewardContractsPerToken[token].add(_rewardContract), '_rewardContract is added');
+
+    emit RewardContractAdded(_rewardContract, true);
+  }
+
+  /**
+   * @notice Remove a whitelisted rewards contract
+   */
+  function removeRewardsContract(IERC20 token, address _rewardContract) external onlyAdmin {
+    require(rewardContractsPerToken[token].remove(_rewardContract), '_rewardContract is removed');
+
+    emit RewardContractAdded(_rewardContract, false);
+  }
+
+  function setVestingDuration(IERC20 token, uint64 _vestingDuration) external onlyAdmin {
+    vestingDurationPerToken[token] = _vestingDuration;
+
+    emit SetVestingDuration(token, _vestingDuration);
+  }
+
+  function lock(
+    IERC20 token,
+    address account,
+    uint256 quantity
+  ) external payable override {
+    lockWithStartBlock(token, account, quantity, _blockNumber());
+  }
+
+  function lockWithStartBlock(
+    IERC20 token,
+    address account,
+    uint256 quantity,
+    uint256 startBlock
+  ) public payable override onlyRewardsContract(token) {
+    require(quantity > 0, 'Quantity cannot be zero');
+
+    // if (token == IERC20(0)) {
+    //   require(msg.value == quantity, 'Invalid locked quantity');
+    // } else {
+    //   // transfer token from reward contract to lock contract
+    //   token.safeTransferFrom(msg.sender, address(this), quantity);
+    // }
+
+    VestingSchedules storage schedules = accountVestingSchedules[account][token];
+    uint256 schedulesLength = schedules.length;
+    uint256 endBlock = startBlock.add(vestingDurationPerToken[token]);
+
+    // combine with the last schedule if they have the same start & end blocks
+    if (schedulesLength > 0) {
+      VestingSchedule storage lastSchedule = schedules.data[schedulesLength - 1];
+      if (lastSchedule.startBlock == startBlock && lastSchedule.endBlock == endBlock) {
+        lastSchedule.quantity = uint256(lastSchedule.quantity).add(quantity).toUint128();
+        accountEscrowedBalance[account][token] = accountEscrowedBalance[account][token].add(quantity);
+        lifetimeLockedTotal[token] = lifetimeLockedTotal[token].add(quantity);
+        emit VestingEntryQueued(schedulesLength - 1, token, account, quantity);
+        return;
+      }
+    }
+
+    // append new schedule
+    schedules.data[schedulesLength] = VestingSchedule({
+      startBlock: startBlock.toUint64(),
+      endBlock: endBlock.toUint64(),
+      quantity: quantity.toUint128(),
+      vestedQuantity: 0
+    });
+    schedules.length = schedulesLength + 1;
+    // record total vesting balance of user
+    accountEscrowedBalance[account][token] = accountEscrowedBalance[account][token].add(quantity);
+    lifetimeLockedTotal[token] = lifetimeLockedTotal[token].add(quantity);
+
+    emit VestingEntryCreated(token, account, startBlock, endBlock, quantity, schedulesLength);
+  }
+
+  /**
+   * @dev vest all completed schedules for multiple tokens
+   */
+  function vestCompletedSchedulesForMultipleTokens(IERC20[] calldata tokens)
+    external override
+    returns (uint256[] memory vestedAmounts)
+  {
+    vestedAmounts = new uint256[](tokens.length);
+    for(uint256 i = 0; i < tokens.length; i++) {
+      vestedAmounts[i] = vestCompletedSchedules(tokens[i]);
+    }
+  }
+
+  /**
+   * @dev claim multiple tokens for specific vesting schedule,
+   *      if schedule has not ended yet, claiming amounts are linear with vesting blocks
+   */
+  function vestScheduleForMultipleTokensAtIndices(
+    IERC20[] calldata tokens,
+    uint256[] calldata indices
+  )
+    external override
+    returns (uint256[] memory vestedAmounts)
+  {
+    vestedAmounts = new uint256[](tokens.length);
+    for(uint256 i = 0; i < tokens.length; i++) {
+      vestedAmounts[i] = vestScheduleAtIndices(tokens[i], indices);
+    }
+  }
+
+  /**
+   * @dev claim multiple tokens for range of schedules
+   *      if schedule has not ended yet, claiming amounts are linear with vesting blocks
+   */
+  function vestScheduleForMultipleTokensInRange(
+    IERC20[] calldata tokens,
+    uint256 startIndex,
+    uint256 endIndex
+  )
+    external override
+    returns (uint256[] memory vestedAmounts)
+  {
+    vestedAmounts = new uint256[](tokens.length);
+    for(uint256 i = 0; i < tokens.length; i++) {
+      vestedAmounts[i] = vestSchedulesInRange(tokens[i], startIndex, endIndex);
+    }
+  }
+  /**
+   * @dev Allow a user to vest all ended schedules
+   */
+  function vestCompletedSchedules(IERC20 token) public override returns (uint256) {
+    VestingSchedules storage schedules = accountVestingSchedules[msg.sender][token];
+    uint256 schedulesLength = schedules.length;
+
+    uint256 totalVesting = 0;
+    for (uint256 i = 0; i < schedulesLength; i++) {
+      VestingSchedule memory schedule = schedules.data[i];
+      if (_blockNumber() < schedule.endBlock) {
+        continue;
+      }
+      uint256 vestQuantity = uint256(schedule.quantity).sub(schedule.vestedQuantity);
+      if (vestQuantity == 0) {
+        continue;
+      }
+      schedules.data[i].vestedQuantity = schedule.quantity;
+      totalVesting = totalVesting.add(vestQuantity);
+
+      emit Vested(token, msg.sender, vestQuantity, i);
+    }
+    _completeVesting(token, totalVesting);
+
+    return totalVesting;
+  }
+
+  /**
+   * @notice Allow a user to vest with specific schedule
+   */
+  function vestScheduleAtIndices(IERC20 token, uint256[] memory indexes)
+    public
+    override
+    returns (uint256)
+  {
+    VestingSchedules storage schedules = accountVestingSchedules[msg.sender][token];
+    uint256 schedulesLength = schedules.length;
+    uint256 totalVesting = 0;
+    for (uint256 i = 0; i < indexes.length; i++) {
+      require(indexes[i] < schedulesLength, 'invalid schedule index');
+      VestingSchedule memory schedule = schedules.data[indexes[i]];
+      uint256 vestQuantity = _getVestingQuantity(schedule);
+      if (vestQuantity == 0) {
+        continue;
+      }
+      schedules.data[indexes[i]].vestedQuantity = uint256(schedule.vestedQuantity)
+        .add(vestQuantity)
+        .toUint128();
+
+      totalVesting = totalVesting.add(vestQuantity);
+
+      emit Vested(token, msg.sender, vestQuantity, indexes[i]);
+    }
+    _completeVesting(token, totalVesting);
+    return totalVesting;
+  }
+
+  function vestSchedulesInRange(IERC20 token, uint256 startIndex, uint256 endIndex)
+    public
+    override
+    returns (uint256)
+  {
+    require(startIndex <= endIndex, 'startIndex > endIndex');
+    uint256[] memory indexes = new uint256[](endIndex - startIndex + 1);
+    for (uint256 index = startIndex; index <= endIndex; index++) {
+      indexes[index - startIndex] = index;
+    }
+    return vestScheduleAtIndices(token, indexes);
+  }
+
+  /* ========== VIEW FUNCTIONS ========== */
+
+  /**
+   * @notice The number of vesting dates in an account's schedule.
+   */
+  function numVestingSchedules(address account, IERC20 token)
+    external
+    override
+    view
+    returns (uint256)
+  {
+    return accountVestingSchedules[account][token].length;
+  }
+
+  /**
+   * @dev manually get vesting schedule at index
+   */
+  function getVestingScheduleAtIndex(
+    address account,
+    IERC20 token,
+    uint256 index
+  ) external override view returns (VestingSchedule memory) {
+    return accountVestingSchedules[account][token].data[index];
+  }
+
+  /**
+   * @dev Get all schedules for an account.
+   */
+  function getVestingSchedules(address account, IERC20 token)
+    external
+    override
+    view
+    returns (VestingSchedule[] memory schedules)
+  {
+    uint256 schedulesLength = accountVestingSchedules[account][token].length;
+    schedules = new VestingSchedule[](schedulesLength);
+    for (uint256 i = 0; i < schedulesLength; i++) {
+      schedules[i] = accountVestingSchedules[account][token].data[i];
+    }
+  }
+
+  function getRewardContractsPerToken(IERC20 token)
+    external
+    view
+    returns (address[] memory rewardContracts)
+  {
+    rewardContracts = new address[](rewardContractsPerToken[token].length());
+    for (uint256 i = 0; i < rewardContracts.length; i++) {
+      rewardContracts[i] = rewardContractsPerToken[token].at(i);
+    }
+  }
+
+  /* ========== INTERNAL FUNCTIONS ========== */
+
+  function _completeVesting(IERC20 token, uint256 totalVesting) internal {
+    require(totalVesting != 0, '0 vesting amount');
+    accountEscrowedBalance[msg.sender][token] = accountEscrowedBalance[msg.sender][token].sub(
+      totalVesting
+    );
+    accountVestedBalance[msg.sender][token] = accountVestedBalance[msg.sender][token].add(
+      totalVesting
+    );
+    lifetimeVestedTotal[token] = lifetimeVestedTotal[token].add(totalVesting);
+
+    if (token == IERC20(0)) {
+      (bool success, ) = msg.sender.call{ value: totalVesting }('');
+      require(success, 'fail to transfer');
+    } else {
+      token.safeTransfer(msg.sender, totalVesting);
+    }
+  }
+
+  /**
+   * @dev implements linear vesting mechanism
+   */
+  function _getVestingQuantity(VestingSchedule memory schedule) internal view returns (uint256) {
+    if (_blockNumber() >= uint256(schedule.endBlock)) {
+      return uint256(schedule.quantity).sub(schedule.vestedQuantity);
+    }
+    if (_blockNumber() <= uint256(schedule.startBlock)) {
+      return 0;
+    }
+    uint256 lockDuration = uint256(schedule.endBlock).sub(schedule.startBlock);
+    uint256 passedDuration = _blockNumber() - uint256(schedule.startBlock);
+    return passedDuration.mul(schedule.quantity).div(lockDuration).sub(schedule.vestedQuantity);
+  }
+
+  /**
+   * @dev wrap block.number so we can easily mock it
+   */
+  function _blockNumber() internal virtual view returns (uint256) {
+    return block.number;
+  }
+}
--- contracts/VaultProxy.sol
+++ contracts/VaultProxy.sol
@@ -1,7 +1,11 @@
-pragma solidity ^0.5.16;
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity 0.7.6;
+
 
 import "./VaultStorage.sol";
 import "./VaultErrorReporter.sol";
+
 
 contract VaultProxy is VaultAdminStorage, VaultErrorReporter {
 
@@ -25,52 +29,57 @@ contract VaultProxy is VaultAdminStorage, VaultErrorReporter {
       */
     event NewAdmin(address oldAdmin, address newAdmin);
 
-    constructor() public {
-        // Set admin to caller
-        admin = msg.sender;
+
+    constructor() {
+        // Set admin to deployer
+        _setAdmin(msg.sender);
     }
 
-    /*** Admin Functions ***/
-    function _setPendingImplementation(address newPendingImplementation) public returns (uint) {
 
-        if (msg.sender != admin) {
-            return fail(Error.UNAUTHORIZED, FailureInfo.SET_PENDING_IMPLEMENTATION_OWNER_CHECK);
+    /**
+     * @dev Modifier used internally that will delegate the call to the implementation unless the sender is the admin.
+     */
+    modifier ifAdmin() {
+        if (msg.sender == _getAdmin()) {
+            _;
+        } else {
+            _delegate();
         }
+    }
 
-        address oldPendingImplementation = pendingVaultImplementation;
 
-        pendingVaultImplementation = newPendingImplementation;
+    /***   Public Functions   ***/
 
-        emit NewPendingImplementation(oldPendingImplementation, pendingVaultImplementation);
-
-        return uint(Error.NO_ERROR);
+    /**
+    * @notice Administrator for this contract
+    */
+    function getAdmin() public view returns (address) {
+        return _getAdmin();
     }
 
     /**
-    * @notice Accepts new implementation of Rifi Vault. msg.sender must be pendingImplementation
-    * @dev Admin function for new implementation to accept it's role as implementation
-    * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+    * @notice Pending administrator for this contract
     */
-    function _acceptImplementation() public returns (uint) {
-        // Check caller is pendingImplementation
-        if (msg.sender != pendingVaultImplementation) {
-            return fail(Error.UNAUTHORIZED, FailureInfo.ACCEPT_PENDING_IMPLEMENTATION_ADDRESS_CHECK);
-        }
-
-        // Save current values for inclusion in log
-        address oldImplementation = vaultImplementation;
-        address oldPendingImplementation = pendingVaultImplementation;
-
-        vaultImplementation = pendingVaultImplementation;
-
-        pendingVaultImplementation = address(0);
-
-        emit NewImplementation(oldImplementation, vaultImplementation);
-        emit NewPendingImplementation(oldPendingImplementation, pendingVaultImplementation);
-
-        return uint(Error.NO_ERROR);
+    function getPendingAdmin() public view returns (address) {
+        return pendingAdmin;
     }
 
+    /**
+    * @notice Active brains of Rifi Vault
+    */
+    function getImplementation() public view returns (address) {
+        return _getImplementation();
+    }
+
+    /**
+    * @notice Pending brains of Rifi Vault
+    */
+    function getPendingImplementation() public view returns (address) {
+        return pendingVaultImplementation;
+    }
+
+
+    /***   Admin Functions   ***/
 
     /**
       * @notice Begins transfer of admin rights. The newPendingAdmin must call `_acceptAdmin` to finalize the transfer.
@@ -80,13 +89,12 @@ contract VaultProxy is VaultAdminStorage, VaultErrorReporter {
       */
     function _setPendingAdmin(address newPendingAdmin) public returns (uint) {
         // Check caller = admin
-        if (msg.sender != admin) {
+        if (msg.sender != _getAdmin()) {
             return fail(Error.UNAUTHORIZED, FailureInfo.SET_PENDING_ADMIN_OWNER_CHECK);
         }
 
         // Save current value, if any, for inclusion in log
         address oldPendingAdmin = pendingAdmin;
-
         // Store pendingAdmin with value newPendingAdmin
         pendingAdmin = newPendingAdmin;
 
@@ -112,33 +120,139 @@ contract VaultProxy is VaultAdminStorage, VaultErrorReporter {
         address oldPendingAdmin = pendingAdmin;
 
         // Store admin with value pendingAdmin
-        admin = pendingAdmin;
-
+        _setAdmin(pendingAdmin);
         // Clear the pending value
         pendingAdmin = address(0);
 
-        emit NewAdmin(oldAdmin, admin);
+        emit NewAdmin(oldAdmin, _getAdmin());
         emit NewPendingAdmin(oldPendingAdmin, pendingAdmin);
 
         return uint(Error.NO_ERROR);
     }
 
     /**
+      * @notice Begins upgrading to new logic contract. The newPendingImplementation must call `_acceptImplementation` to finalize the transfer.
+      * @param newPendingImplementation New pending implementation.
+      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+      */
+    function _setPendingImplementation(address newPendingImplementation) public returns (uint) {
+        if (msg.sender != _getAdmin()) {
+            return fail(Error.UNAUTHORIZED, FailureInfo.SET_PENDING_IMPLEMENTATION_OWNER_CHECK);
+        }
+
+        address oldPendingImplementation = pendingVaultImplementation;
+        pendingVaultImplementation = newPendingImplementation;
+
+        emit NewPendingImplementation(oldPendingImplementation, pendingVaultImplementation);
+
+        return uint(Error.NO_ERROR);
+    }
+
+    /**
+      * @notice Accepts new implementation of Rifi Vault. msg.sender must be pendingImplementation
+      * @dev Admin function for new implementation to accept it's role as implementation
+      * @return uint 0=success, otherwise a failure (see ErrorReporter.sol for details)
+      */
+    function _acceptImplementation() public returns (uint) {
+        // Check caller is pendingImplementation
+        if (msg.sender != pendingVaultImplementation) {
+            return fail(Error.UNAUTHORIZED, FailureInfo.ACCEPT_PENDING_IMPLEMENTATION_ADDRESS_CHECK);
+        }
+
+        // Save current values for inclusion in log
+        address oldImplementation = vaultImplementation;
+        address oldPendingImplementation = pendingVaultImplementation;
+
+        // Store new implementation and clear pending
+        _setImplementation(pendingVaultImplementation);
+        pendingVaultImplementation = address(0);
+
+        emit NewImplementation(oldImplementation, _getImplementation());
+        emit NewPendingImplementation(oldPendingImplementation, pendingVaultImplementation);
+
+        return uint(Error.NO_ERROR);
+    }
+
+
+    /**
      * @dev Delegates execution to an implementation contract.
      * It returns to the external caller whatever the implementation returns
      * or forwards reverts.
      */
-    function () external payable {
+    function _delegate() internal {
         // delegate all other functions to current implementation
-        (bool success, ) = vaultImplementation.delegatecall(msg.data);
+        (bool success, ) = _getImplementation().delegatecall(msg.data);
 
         assembly {
-              let free_mem_ptr := mload(0x40)
-              returndatacopy(free_mem_ptr, 0, returndatasize)
+            let free_mem_ptr := mload(0x40)
+            returndatacopy(free_mem_ptr, 0, returndatasize())
 
-              switch success
-              case 0 { revert(free_mem_ptr, returndatasize) }
-              default { return(free_mem_ptr, returndatasize) }
+            switch success
+            case 0 { revert(free_mem_ptr, returndatasize()) }
+            default { return(free_mem_ptr, returndatasize()) }
         }
+    }
+
+    /**
+     * @dev Makes sure the admin cannot access the fallback function.
+     */
+    function _fallback() internal {
+        // require(msg.sender != _getAdmin(), "TransparentUpgradeableProxy: admin cannot fallback to proxy target");
+        _delegate();
+    }
+
+    /**
+     * @dev Fallback function that delegates calls to implementation. Will run if call data is empty.
+     */
+    receive() external payable {
+        _fallback();
+    }
+
+    /**
+     * @dev Fallback function that delegates calls to implementation. Will run if no other function in the contract
+     * matches the call data.
+     */
+    fallback() external payable {
+        _fallback();
+    }
+
+    /***   Internal helpers   ***/
+
+    /**
+     * @dev Returns the current admin.
+     */
+    function _getAdmin() internal view returns (address) {
+        return admin;
+    }
+
+    /**
+     * @dev Set new admin.
+     */
+    function _setAdmin(address _newAdmin) internal {
+        admin = _newAdmin;
+    }
+
+    /**
+     * @dev Returns the current implementation.
+     */
+    function _getImplementation() internal view returns (address) {
+        return vaultImplementation;
+    }
+
+    /**
+     * @dev Set new implementation.
+     */
+    function _setImplementation(address _newImpl) internal {
+        vaultImplementation = _newImpl;
+    }
+}
+
+abstract contract VaultImplementation {
+    /**
+     * @dev Become the new brain of proxy
+     */
+    function _become(VaultProxy proxy) public {
+        require(msg.sender == proxy.getAdmin(), "_become: only proxy admin can change brains");
+        require(proxy._acceptImplementation() == 0, "_become: change not authorized");
     }
 }
